5e92c379efa649dfee906117e264c68faad68e2d
Jonathan <jonathan.fermanm@gmail.com>
2023-06-15 18:02:05 -0600

    1

--- stock_project/resources/views/pages/registerProduct.blade.php
+++ stock_project/resources/views/pages/registerProduct.blade.php
@@ -3,35 +3,78 @@
 <!-- Haciendo referencia al yield de template.blade.php -->
 
 @section('dynamic_body')
-<div class="container-fluid p-3 col-auto col-md"> 
-    <h1 class="text-center text-primary">Add Products</h1>
+<div class="container-fluid col-auto col-md"> 
+    
 
     <!-- haciendo un llamado al name de la ruta de saveCourse -->
     <form action="{{ route('saveProduct') }}" method="POST">
         <!-- solicitamos token para el envio de datos -->
         @csrf
-        <label for="">Name</label>
-        <input type="text" name="name" class="form-control">
+        <div class="body  d-flex col-auto col-md"> 
+                     <div class="container">
+                              <div class="cover">
+                                  <div class="front">
+                                    <img src="Resource/storage.jpg" alt="">
+                                    
+                                  </div>
+                              </div>
 
-        <label for="">Select a measurement unit</label>
-        <select name="measure" class="form-control">
-            @foreach ($measure as $element)
-                <option value="{{ $element->id }}">{{ $element->name }}</option>
-            @endforeach
-        </select>
+                              <div class="forms">
+                                  <div class="form-content">
 
-        <label for="">Price</label>
-        <input type="text" name="price" class="form-control">
+                                    <div class="login-form">
+                                          <div class="title">Add Products</div>
+                                            <form action="#">
+                                                <div class="input-boxes">
+                                                  
+                                                  <div class="input-box"> 
+                                                    <label for="name"> Name </label>
+                                                    <input type="text" class="" id="name">
+                                                    
+                                                  
+                                                  </div>
+                                                  
+                                                  <div class="input-box">
+                                                     
+                                                        <select for="lastname" name="measure" class="form-control"> Unit 
+                                                        
+                                                        @foreach ($measure as $element)
+                                                        <option value="{{ $element->id }}">{{ $element->name }}</option>
+                                                                @endforeach
+                                                        </select>
+                                                        <input type="text" class="" id="lastname">
+                                                   </div>   
+                                                  
+                                                  </div>
+                                                  
+                                                  <div class="input-box"> 
+                                                    <label for=""> Price </label>
+                                                   
+                                                    <input type="text" class="i" id="price" placeholder="$">
+                                                    
+                                                  
+                                                  </div>
+                                                  
+                                                  <div class="input-box"> 
+                                                    <label for="lastname"> Supplier </label>
+                                                    <select name="supplier" class="form-control">
+                                                        @foreach ($supplier as $elements)
+                                                            <option value="{{ $elements->id }}">{{ $elements->name }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                  
+                                                  </div>
+                                                  
+                                                  <input type="submit" class="btn btn-primary mt-3" value="Guardar Datos">
+                                          
+                                        </form>
 
-        <label for="">Select a supplier</label>
-        <select name="supplier" class="form-control">
-            @foreach ($supplier as $elements)
-                <option value="{{ $elements->id }}">{{ $elements->name }}</option>
-            @endforeach
-        </select>
-
-        
-        <input type="submit" class="btn btn-success mt-3" value="Guardar Datos">
+                                    </div>
+                                
+                                </div>
+                              </div>
+                       </div>
+              </div>
     </form>
 </div>
 @endsection
